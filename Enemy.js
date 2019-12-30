3de780246d7659bad7bbbb016346238db39a8de6
spiderpiggy03 <49546430+spiderpiggy03@users.noreply.github.com>
2019-12-29 19:42:24 -0500

    Before enemy angle update

--- Enemy.js
+++ Enemy.js
@@ -18,7 +18,7 @@ class Enemy {
 
         this.rotate(direction);
 
-        this.heading = 4;
+        this.heading = 0;
 
         this.score = score;
         this.health = health;
@@ -31,7 +31,7 @@ class Enemy {
     }
 
     move() {
-        const theta = enemyAngles[this.heading];
+        const theta = this.heading;
 
         const newMapPosX = Math.floor(((this.x * scale) + (Math.cos(theta) * MoveSpeed) * 2) / scale);
         const newMapPosY = Math.floor(((this.y * scale) + (Math.sin(theta) * -1 * MoveSpeed) * 2) / scale);
@@ -74,7 +74,7 @@ class Enemy {
             this.y += Math.sin(theta) * -1 * this.moveSpeed;
 
 
-        } else if (World[newMapPosY][mapPosX] === 0 && !spriteHitY && Math.abs(Math.sin(enemyAngles[this.heading])) > 0.00001) {
+        } else if (World[newMapPosY][mapPosX] === 0 && !spriteHitY && Math.abs(Math.sin(theta)) > 0.00001) {
             this.y += Math.sin(theta) * -1 * this.moveSpeed;
 
         } else if (World[mapPosY][newMapPosX] === 0 && !spriteHitX) {
@@ -88,16 +88,41 @@ class Enemy {
 
     rotate(direction) {
 
-        if (direction > 0) {
-            const elements = this.dir.splice(-direction, direction);
-            this.dir = elements.concat(this.dir);
-            this.heading += direction;
-            while (this.heading > 8) this.heading -= 8;
-        } else {
-            const elements = this.dir.splice(0, Math.abs(direction));
-            this.dir = this.dir.concat(elements);
-            this.heading += direction;
-            while (this.heading < 0) this.heading += 8;
+        // if (direction > 0) {
+        //     const elements = this.dir.splice(-direction, direction);
+        //     this.dir = elements.concat(this.dir);
+        //     this.heading += direction;
+        //     while (this.heading > 8) this.heading -= 8;
+        // } else {
+        //     const elements = this.dir.splice(0, Math.abs(direction));
+        //     this.dir = this.dir.concat(elements);
+        //     this.heading += direction;
+        //     while (this.heading < 0) this.heading += 8;
+        // }
+
+
+        this.heading += direction;
+        while (this.heading > Math.PI) this.heading -= Math.PI * 2;
+        while (this.heading < Math.PI) this.heading += Math.PI * 2;
+
+        const dir = this.heading;
+
+        if (dir > 7 * Math.PI / 8 || (dir < -7 * Math.PI / 8 && dir > -Math.PI)) {
+            playerDirection = this.dir[0]
+        } else if (dir > -7 * Math.PI / 8 && dir < -5 * Math.PI / 8) {
+            playerDirection = this.dir[1]
+        } else if (dir > -5 * Math.PI / 8 && dir < -3 * Math.PI / 8) {
+            playerDirection = this.dir[2]
+        } else if (dir > -3 * Math.PI / 8 && dir < -1 * Math.PI / 8) {
+            playerDirection = this.dir[3]
+        } else if ((dir > -1 * Math.PI / 8 && dir < 0) || (dir < 1 * Math.PI / 8 && dir > 0)) {
+            playerDirection = this.dir[4]
+        } else if (dir > 1 * Math.PI / 8 && dir < 3 * Math.PI / 8) {
+            playerDirection = this.dir[5]
+        } else if (dir > 3 * Math.PI / 8 && dir < 5 * Math.PI / 8) {
+            playerDirection = this.dir[6]
+        } else if (dir > 5 * Math.PI / 8 && dir < 7 * Math.PI / 8) {
+            playerDirection = this.dir[7]
         }
 
     }
@@ -113,8 +138,8 @@ class Enemy {
 
     inSight() {
 
-        const playerX =  player.pos.x / scale;
-        const playerY =  player.pos.y / scale;
+        const playerX = player.pos.x / scale;
+        const playerY = player.pos.y / scale;
 
         const dX = playerX - this.x;
         const dY = playerY - this.y;
@@ -141,20 +166,22 @@ class Enemy {
             playerDirection = this.dir[7]
         }
 
-        if (playerDirection === "guardStandFront" || playerDirection === "guardStandFrontLeft"  || playerDirection === "guardStandFrontRight" ) return true;
+        if (playerDirection === "guardStandFront" || playerDirection === "guardStandFrontLeft" || playerDirection === "guardStandFrontRight") return true;
 
         return false;
     }
 
     search() {
-        const playerX =  player.pos.x / scale;
-        const playerY =  player.pos.y / scale;
+        const playerX = player.pos.x / scale;
+        const playerY = player.pos.y / scale;
 
         const dX = playerX - this.x;
         const dY = playerY - this.y;
 
         const dir = Math.atan2(dY, dX);
 
-        this.rotate( Math.floor(dir / (Math.PI / 8)) - this.heading);
+        const rotation = Math.round(dir / (Math.PI / 8)) - this.heading;
+        console.log(dir * (180 / Math.PI), rotation, this.heading)
+        this.rotate(rotation);
     }
 }
